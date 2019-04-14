68c71982c231bbe76de20900550219f43724e359
awrohan <awrohan@users.noreply.github.com>
2019-04-14 16:25:10 -0500

    Added shopping cart dialog struts

--- FrontEnd/PostOfficeAngularSite/src/app/shop/shop.component.ts
+++ FrontEnd/PostOfficeAngularSite/src/app/shop/shop.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
-export interface DialogData {
-  animal: string;
-  name: string;
-}
-
 export interface Item {
   title: string;
   description: string;
   image: string;
-  price: string;
+  price: number;
+}
+
+export interface CartItem {
+  item: Item;
+  quantity: number;
 }
 
 @Component({
@@ -21,10 +21,29 @@ export interface Item {
 export class ShopComponent implements OnInit {
   constructor(public dialog: MatDialog) {}
 
+  items: Item[] = [
+    {title:       'Thingamajig',
+     description: 'it does something',
+     image:       'assets/shop_images/box1.jpg',
+     price:       500},
+    {title:       'Foobar',
+     description: 'it does something',
+     image:       'assets/shop_images/box2.jpg',
+     price:       500},
+    {title:       'Placeholder',
+     description: 'it does something',
+     image:       'assets/shop_images/box3.jpg',
+     price:       500},
+    {title:       'Whocares',
+     description: 'it does something',
+     image:       'assets/shop_images/box4.jpg',
+     price:       500},
+  ];
+
   openCart(): void {
     const dialogRef = this.dialog.open(ShoppingCartDialog, {
-      width: '250px',
-      data: {name: "john", animal: "cat"}
+      width: '600px',
+      data: [{item: this.items[0], quantity: 1}]
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -35,36 +54,18 @@ export class ShopComponent implements OnInit {
   ngOnInit() {
   }
 
-  items: Item[] = [
-    {title:       'Thingamajig',
-     description: 'it does something',
-     image:       'assets/shop_images/box1.jpg',
-     price:       '$500'},
-    {title:       'Foobar',
-     description: 'it does something',
-     image:       'assets/shop_images/box2.jpg',
-     price:       '$500'},
-    {title:       'Placeholder',
-     description: 'it does something',
-     image:       'assets/shop_images/box3.jpg',
-     price:       '$500'},
-    {title:       'Whocares',
-     description: 'it does something',
-     image:       'assets/shop_images/box4.jpg',
-     price:       '$500'},
-  ];
-
 }
 
 @Component({
   selector: 'shopping-cart-dialog',
   templateUrl: './shopping-cart-dialog.html',
+  styleUrls: ['./shop.component.css']
 })
 export class ShoppingCartDialog {
 
   constructor(
     public dialogRef: MatDialogRef<ShoppingCartDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: CartItem[]) {}
 
   onNoClick(): void {
     this.dialogRef.close();
