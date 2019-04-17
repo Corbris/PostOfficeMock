db8ee52ffb5d09e2ea3890edc9515a96f4cd0ad9
jac.massa0908@gmail.com <jac.massa0908@gmail.com>
2019-04-17 15:25:06 -0500

    Checkout page completed

--- FrontEnd/PostOfficeAngularSite/src/app/checkout/checkout.component.ts
+++ FrontEnd/PostOfficeAngularSite/src/app/checkout/checkout.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { cartItems, unsealCart, sealCart } from '../shop/shop.component';
+import {CartItem, cartItems, unsealCart, sealCart } from '../shop/shop.component';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
+
 export class CheckoutComponent implements OnInit {
 
   constructor() { }
@@ -16,6 +17,22 @@ export class CheckoutComponent implements OnInit {
       result += cartItems[i].quantity;
     }
     return result;
+  }
+
+  getCartItems() {
+    return cartItems;
+  }
+  computePrice(i: CartItem): number {
+    return i.item.price * i.quantity;
+  }
+
+  computeTotal() {
+    var total = 0;
+    for (var i in cartItems) {
+      total += this.computePrice(cartItems[i]);
+    }
+    return total;
+    // TODO: Tax?
   }
 
   ngOnInit() {
