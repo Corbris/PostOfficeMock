a2b8cf97d1fab4f02adf0a78ff1012498fa29e46
awrohan <awrohan@users.noreply.github.com>
2019-04-17 18:01:13 -0500

    added validation, checkout function

--- FrontEnd/PostOfficeAngularSite/src/app/checkout/checkout.component.ts
+++ FrontEnd/PostOfficeAngularSite/src/app/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CartItem, cartItems, unsealCart, sealCart } from '../shop/shop.component';
 import { APIService } from '../_services/api.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-checkout',
@@ -15,7 +16,9 @@ export class CheckoutComponent implements OnInit {
   CustomerData;
   UserID = sessionStorage.getItem("ID");
 
-  constructor(public api: APIService) { }
+  constructor(private formBuilder: FormBuilder,
+              public api: APIService,
+              private snackBar: MatSnackBar) { }
 
   getNumCartItems(): number {
     var result = 0;
@@ -41,24 +44,65 @@ export class CheckoutComponent implements OnInit {
     // TODO: Tax?
   }
 
+  checkoutComplete() {
+    if (!this.userCheckoutForm.valid) {
+      this.snackBar.open("Invalid billing data", "Close", {
+        duration: 8000,
+      });
+    }
+    console.log("something happened");
+    // TODO: empty cart
+  }
+
   ngOnInit() {
     unsealCart();
 
+    this.userCheckoutForm = this.formBuilder.group({
+      Fname: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)])],
+      Mint: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(1)])],
+      Lname: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)])],
+      Email: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)])],
+      PhoneNumber: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)])],
+      HouseNumber: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)])],
+      Street: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)])],
+      City: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)])],
+      State: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)])],
+      Zip: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(2)])]
+    });
 
     this.api.customerFromID(sessionStorage.getItem("ID"))
-    .subscribe((Data) => {
-      this.CustomerData = Data[0];
-      this.userCheckoutForm.controls.Fname.setValue(this.CustomerData.Fname);
-      this.userCheckoutForm.controls.Lname.setValue(this.CustomerData.Lname);
-      this.userCheckoutForm.controls.Email.setValue(this.CustomerData.Email);
-      this.userCheckoutForm.controls.PhoneNumber.setValue(this.CustomerData.MobileNumber);
-      this.userCheckoutForm.controls.HouseNumber.setValue(this.CustomerData.HouseNumber);
-      this.userCheckoutForm.controls.Street.setValue(this.CustomerData.Street);
-      this.userCheckoutForm.controls.City.setValue(this.CustomerData.City);
-      this.userCheckoutForm.controls.State.setValue(this.CustomerData.State);
-      this.userCheckoutForm.controls.Zip.setValue(this.CustomerData.ZipCode);
+      .subscribe((Data) => {
+        this.CustomerData = Data[0];
+        this.userCheckoutForm.controls.Fname.setValue(this.CustomerData.Fname);
+        this.userCheckoutForm.controls.Lname.setValue(this.CustomerData.Lname);
+        this.userCheckoutForm.controls.Email.setValue(this.CustomerData.Email);
+        this.userCheckoutForm.controls.PhoneNumber.setValue(this.CustomerData.MobileNumber);
+        this.userCheckoutForm.controls.HouseNumber.setValue(this.CustomerData.HouseNumber);
+        this.userCheckoutForm.controls.Street.setValue(this.CustomerData.Street);
+        this.userCheckoutForm.controls.City.setValue(this.CustomerData.City);
+        this.userCheckoutForm.controls.State.setValue(this.CustomerData.State);
+        this.userCheckoutForm.controls.Zip.setValue(this.CustomerData.ZipCode);
 
     });
   }
-
 }
