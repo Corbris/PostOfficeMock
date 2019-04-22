5a0fb2141a036380fdcbbb78326cf36019cab06b
Kevin Lucas <ktlucas03@gmail.com>
2019-04-22 11:09:37 -0500

    added checkout api

--- FrontEnd/PostOfficeAngularSite/src/app/checkout/checkout.component.ts
+++ FrontEnd/PostOfficeAngularSite/src/app/checkout/checkout.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CartItem, cartItems, unsealCart, sealCart } from '../shop/shop.component';
+import { CartItem, cartItems, unsealCart, sealCart } from '../shop/shop.component';
 import { APIService } from '../_services/api.service';
 import { MatSnackBar } from '@angular/material';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-checkout',
@@ -13,12 +14,12 @@ import { MatSnackBar } from '@angular/material';
 export class CheckoutComponent implements OnInit {
 
   userCheckoutForm: FormGroup;
-  CustomerData;
-  UserID = sessionStorage.getItem("ID");
+  showcart = true;
 
   constructor(private formBuilder: FormBuilder,
               public api: APIService,
               private snackBar: MatSnackBar) { }
+
 
   getNumCartItems(): number {
     var result = 0;
@@ -35,11 +36,14 @@ export class CheckoutComponent implements OnInit {
     return i.item.price * i.quantity;
   }
 
+  Total;
   computeTotal() {
     var total = 0;
     for (var i in cartItems) {
       total += this.computePrice(cartItems[i]);
     }
+    total = total + total * .08;
+    this.Total = '' + total;
     return total;
     // TODO: Tax?
   }
@@ -47,62 +51,78 @@ export class CheckoutComponent implements OnInit {
   checkoutComplete() {
     if (!this.userCheckoutForm.valid) {
       this.snackBar.open("Invalid billing data", "Close", {
-        duration: 8000,
+        duration: 3000,
       });
+      console.log(this.userCheckoutForm);
+
+
     }
+    else { 
     console.log("something happened");
-    // TODO: empty cart
+    console.log(this.userCheckoutForm);
+    //make transaction
+
+    this.api.onlineTransaction(sessionStorage.getItem("ID"), formatDate(new Date(), 'yyyy-MM-dd HH:MM:SS', 'en'), this.userCheckoutForm.value.CCnumber.substring(0,4) ,''+this.Total, this.userCheckoutForm.value.FnameCC, this.userCheckoutForm.value.LnameCC, this.userCheckoutForm.value.MInitCC, this.userCheckoutForm.value.CardType)
+      .subscribe((res) => {
+        console.log(res);
+        console.log("made transaction");
+        if (res != null || res != undefined) {
+          console.log(res.insertId);
+          for (var i in cartItems) {
+            this.api.orderDetails(cartItems[i].item.title, res.insertId, ''+cartItems[i].quantity, ''+cartItems[i].item.price)
+              .subscribe((res) => {
+                console.log(res);
+                console.log(cartItems[i].item.title, cartItems[i].item.price, cartItems[i].quantity);
+
+                
+              });
+          }
+          //done with cart
+          this.clearCart();
+          //show transactions
+        }        
+      });;
+  }
+  }
+
+  clearCart() {
+    
+    let temp = localStorage.getItem("carts");
+    temp = JSON.parse(temp);
+    temp[0]['cartItems'] = [];
+    localStorage.setItem("carts", JSON.stringify(temp));
+
+    //this reload page
+
+    this.snackBar.open("Thanks for your purchase", "Close", {
+      duration: 3000,
+
+    });
+    
+    
+  }
+
+  show() {
+
   }
 
   ngOnInit() {
     unsealCart();
 
     this.userCheckoutForm = this.formBuilder.group({
-      Fname: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(2)])],
-      Mint: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(1)])],
-      Lname: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(2)])],
-      Email: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(2)])],
-      PhoneNumber: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(2)])],
-      HouseNumber: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(2)])],
-      Street: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(2)])],
-      City: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(2)])],
-      State: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(2)])],
-      Zip: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(2)])]
-    });
-
-    this.api.customerFromID(sessionStorage.getItem("ID"))
-      .subscribe((Data) => {
-        this.CustomerData = Data[0];
-        this.userCheckoutForm.controls.Fname.setValue(this.CustomerData.Fname);
-        this.userCheckoutForm.controls.Lname.setValue(this.CustomerData.Lname);
-        this.userCheckoutForm.controls.Email.setValue(this.CustomerData.Email);
-        this.userCheckoutForm.controls.PhoneNumber.setValue(this.CustomerData.MobileNumber);
-        this.userCheckoutForm.controls.HouseNumber.setValue(this.CustomerData.HouseNumber);
-        this.userCheckoutForm.controls.Street.setValue(this.CustomerData.Street);
-        this.userCheckoutForm.controls.City.setValue(this.CustomerData.City);
-        this.userCheckoutForm.controls.State.setValue(this.CustomerData.State);
-        this.userCheckoutForm.controls.Zip.setValue(this.CustomerData.ZipCode);
-
+      FnameCC: [''],
+      MInitCC: [''],
+      LnameCC: [''],
+      CCnumber: [''],
+      CCexpiration: [''],
+      CCcvv: [''],
+      CardType: ['']
     });
   }
+
+
+  
+
+
+
 }
